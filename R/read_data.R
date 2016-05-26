9ced6d97a273b71e6ddb0819e075fdaccfff07e3
Matthew Workentine <matt.workentine@gmail.com>
2016-05-26 15:58:12 -0600

    added new parameter to specify the intensity channel, quits nicely when can't find the section, and added automatic detection of column types based on names

--- R/read_data.R
+++ R/read_data.R
@@ -8,9 +8,10 @@
 #' extraneous columns and extracting the well name and multiple fields of view if present.
 #'
 #' @param path File path of the csv file
-#' @param flag Name of the section to import. Default is 'Calibration'.
+#' @param section Name of the section to import. Default is 'Calibration'.
 #' @param convert_time Conversion factor for the time column.  Default is \eqn{1000 * 60} (converts
 #'   milliseconds to minutes). NULL means no conversion will be performed.
+#' @param channel The channel to use for intensity measurements. Default is 'Ch 3'
 #'
 #' @return dataframe
 #'
@@ -18,18 +19,60 @@
 #'
 #' @export
 
-read_data = function(path, flag = "Calibration", convert_time = 1000 * 60) {
+read_data = function(path, section = "Calibration", convert_time = 1000 * 60, channel = "Ch 3") {
+
   raw = readr::read_lines(path)
-  startidx = which(stringr::str_detect(raw, flag)) + 1
-  markers = which(stringr::str_detect(raw,"=========="))
-  markers = markers[markers > startidx]
-  endidx = length(raw)
+
+  # mark the start of the section of interest and stop if we can't find it
+  startidx = which(stringr::str_detect(raw, section)) + 1
+  if(length(startidx) == 0) stop("Couldn't find section named ", section)
+
+
+  markers = which(stringr::str_detect(raw,"==========")) # find all the other markers
+  markers = markers[markers > startidx]  # get rid of ones above our start
+  endidx = length(raw) # set the end index
+
+  # if we have a marker before the end then this is our new end
   if (min(markers) < endidx) endidx = min(markers) - 1
-  if (length(endidx) != 0) raw = raw[startidx:endidx]
+  if (length(endidx) != 0) raw = raw[startidx:endidx] # get the lines in our section
+
+  # identify the columns we want
+  columns = raw[str_detect(raw, "Section")]
+  channels = raw[str_detect(raw, "Ch")]
+  channels = channels %>% str_split(",") %>% "[["(1)
+  columns = columns %>% str_split(",") %>% "[["(1)
+
+  uniq_channels = unique(channels[channels != ""])
+
+  if (!channel %in% uniq_channels) {
+    stop("Couldn't find channel named ", channel, " Should be one of ", paste(uniq_channels, collapse = ", "))
+  }
+
+  col_types = list()
+
+  # here we create the column type vector using readr's character syntax
+  for (i in 1:length(columns)) {
+    if (columns[i] %in%  c("Section", "Label")) {
+      col_types[i] = "c"
+    } else if (columns[i] == "Dens - Levels" && channels[i] == channel) {
+      col_types[i] = "n"
+    } else if (columns[i] == "Time") {
+      col_types[i] = "n"
+    } else {
+      col_types[i] = "_"
+    }
+  }
+
+  col_types = paste(col_types, collapse = "")
+  #col_names = c("Section", "Label", "Time", "Intensity")
+
+  # parse the data
   raw = paste0(raw, collapse = "\n")
-  col_types = "c____c_____n_n_"
-  col_names = c("Section", "Label", "Time", "Intensity")
-  raw = readr::read_csv(raw, skip = 3, col_names = col_names, col_types = col_types)
+  #raw = readr::read_csv(raw, skip = 2, col_names = col_names, col_types = col_types)
+  raw = readr::read_csv(raw, skip = 1, col_types = col_types) %>%
+    dplyr::rename(Intensity = `Dens - Levels`)
+
+
   if (stringr::str_detect(raw$Section[1], "fld")) {
     message("Detected multiple fields")
     raw = raw %>%
